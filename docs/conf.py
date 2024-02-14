236528b6528d40514ab2f63d9abe444f06e20c92
Garrett Broters <52166834+Garr-R@users.noreply.github.com>
2024-02-14 14:57:22 -0500

    logo test

--- docs/conf.py
+++ docs/conf.py
@@ -60,6 +60,17 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+html_theme_options = {
+    # if we have a html_logo below, this shows /only/ the logo with no title text
+    "logo_only": True,
+    # Collapse navigation (False makes it tree-like)
+    "collapse_navigation": False,
+    # Hide the documentation version name/number under the logo
+    "display_version": False,
+}
+
+html_logo = "img/docs_logo.png"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
